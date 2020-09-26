738686a3b33f8a3e5328113d0e93ba817e39bc4a
neeerajtk <neerajtk560@gmail.com>
2020-09-26 22:49:45 +0530

    navbar items mapped

--- good-day/src/components/Navbar.js
+++ good-day/src/components/Navbar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import '../styles/Navbar.css';
+import {MenuItems} from './MenuItems';
 
 function Navbar() {
     return (
-       <nav className="navbar">
+       <nav className="NavbarItems">
            <h1 className="navbar-logo">Good Day</h1>
+           <div className="menu-icon"></div>
+           <ul>
+               {MenuItems.map((value,key)=> {
+                   return(
+                    <li key={key}><a className={value.cName} href={value.url}>
+                         {value.title}
+                    </a>
+                </li>
+                   )
+               })}
+           </ul>
        </nav>
-    )
+    );
 }
 
 export default Navbar;
