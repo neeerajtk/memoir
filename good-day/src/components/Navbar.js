74277713136e9c0b78967d2527f75c50d808da60
neeerajtk <neerajtk560@gmail.com>
2020-09-26 23:59:44 +0530

    nav styling added

--- good-day/src/components/Navbar.js
+++ good-day/src/components/Navbar.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/Navbar.css';
 import {MenuItems} from './MenuItems';
 
+
+
 function Navbar() {
+    const [click,setClick] = useState(false);
+
+    var handleClick = () => {
+        setClick(!click)    
+    }
+
     return (
        <nav className="NavbarItems">
-           <h1 className="navbar-logo">Good Day</h1>
-           <div className="menu-icon"></div>
-           <ul>
+           <h1 className="navbar-logo"><i class="fas fa-book"></i></h1>
+           <div className="menu-icon" onClick={handleClick}>
+               <i className={click ? 'fas fa-times' : 'fas fa-bars' }></i>
+           </div>
+           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                {MenuItems.map((value,key)=> {
                    return(
                     <li key={key}><a className={value.cName} href={value.url}>
