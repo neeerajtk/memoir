76a13b50b67a9ddfeadac688dc1afc3826e96865
neeerajtk <neerajtk560@gmail.com>
2020-09-26 22:05:47 +0530

    navbar menu items component added

---
+++ good-day/src/components/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems = [
+    {
+        title:"View Style",
+        url:"#",
+        cName:"nav-links"
+    },
+  {
+        title:"Sign Out",
+        url:"#",
+        cName:"nav-links"
+    }
+] ;
---
+++ good-day/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+function Navbar() {
+    return (
+       <nav className="navbar">
+           <h1 className="navbar-logo">Good Day</h1>
+       </nav>
+    )
+}
+
+export default Navbar;
