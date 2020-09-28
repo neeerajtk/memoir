15bbb29ac1b97b5e196eb28d974b67f2285af8ce
neeerajtk <neerajtk560@gmail.com>
2020-09-28 17:37:39 +0530

    nav removed and new temp nav added

---
+++ memoir/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+
+
+function Navbar() {
+    
+
+    return (
+        <nav>  
+        <ul className="container">  
+          <li>Logo</li>  
+          <li>Change View</li>  
+          <li>Login</li>  
+        </ul>  
+      </nav>
+
+
+    );
+}
+
+export default Navbar;
