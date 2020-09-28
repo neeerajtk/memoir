5433c117f9c4a1d09eed6157ccae1d621d294a3d
neeerajtk <neerajtk560@gmail.com>
2020-09-28 21:27:05 +0530

    nav titles added

--- memoir/src/components/Navbar.js
+++ memoir/src/components/Navbar.js
@@ -11,9 +11,9 @@ function Navbar() {
           <h4>Memoir</h4>
         </div>
         <ul className={`nav-links ${menu ? "nav-active": ""}`}>  
-          <li><a href="#"><i class="far fa-lightbulb fa-lg"></i> </a></li>  
-          <li><a href="#"><i class="fas fa-retweet fa-lg"></i> </a></li>  
-          <li><a href="#"><i class="fas fa-key fa-lg"></i> </a></li>  
+          <li><a className="tooltip" title="Switch Dark Mode" href="#"><i className="far fa-lightbulb fa-lg"></i> </a></li>  
+          <li><a href="#" title="Change View"><i className="fas fa-retweet fa-lg"></i> </a></li>  
+          <li><a href="#" title="Login"><i className="fas fa-key fa-lg"></i> </a></li>  
         </ul>  
         <div className="burger" onClick={()=> setMenu(!menu)}>
           <div></div>
