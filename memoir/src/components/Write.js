6502205034fc2601fd965768898e45f4f4f22887
neeerajtk <neerajtk560@gmail.com>
2020-10-01 15:53:06 +0530

    date retreival func added

--- memoir/src/components/Write.js
+++ memoir/src/components/Write.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Card} from 'react-bootstrap';
 import '../styles/Write.css';
 
 
 function Write() {
+    const [date,setDate]= useState(Date().toLocaleString().slice(0,21));
     return (
         <div>
             <Card>
             <Card.Body>
                 <Card.Title>Note Heading</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Date</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">{date}</Card.Subtitle>
                 <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
