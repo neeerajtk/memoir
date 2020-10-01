af54d1fe392a6d98269c8c404dd8b65588fdbb49
neeerajtk <neerajtk560@gmail.com>
2020-10-01 10:42:51 +0530

    nav z index corrected

--- memoir/src/components/Navbar.js
+++ memoir/src/components/Navbar.js
@@ -8,12 +8,12 @@ function Navbar() {
     return (
         <nav className="nav-active">  
         <div className="logo">
-          <h4>Memoir</h4>
+          <h4>SPACE X</h4>
         </div>
         <ul className={`nav-links ${menu ? "nav-active": ""}`}>  
           <li><a href="#" title="Switch Dark Mode"><i className="fas fa-lightbulb fa-lg"></i></a></li>  
-          <li><a href="#" title="Change View"><i className="fas fa-retweet fa-lg"></i> </a></li>  
-          <li><a href="#" title="Login"><i className="fas fa-key fa-lg"></i> </a></li>  
+          <li><a href="#" title="Rerender"><i className="fas fa-retweet fa-lg"></i> </a></li>  
+          <li><a href="#" title="Contact dev"><i className="fas fa-key fa-lg"></i> </a></li>  
         </ul>  
         <div className="burger" onClick={()=> setMenu(!menu)}>
           <div></div>
