db05a288306101afe50024a56c275390ed52b383
neeerajtk <neerajtk560@gmail.com>
2020-10-04 22:58:02 +0530

    params added

--- memoir/src/firebase/farebase.utils.js
+++ memoir/src/firebase/farebase.utils.js
@@ -14,3 +14,10 @@ const config = {
 
   firebase.initializeApp(config);
 
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({prompt:'select_account'});
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default firebase;
