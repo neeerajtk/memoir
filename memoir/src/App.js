ec1d69020f0fa4cbabb0f421c57a4ba3d9cefcb1
neeerajtk <neerajtk560@gmail.com>
2020-10-10 22:39:23 +0530

    sign out on user sign in made working

--- memoir/src/App.js
+++ memoir/src/App.js
@@ -6,27 +6,27 @@ import {Switch, Route, Redirect, BrowserRouter} from 'react-router-dom';
 import Write from './components/Write';
 import SignIn from './components/SignIn';
 import {auth} from './firebase/farebase.utils'; 
-
+ 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unSubscribeFromAuth = null;
 
   componentDidMount(){
     this.unSubscribeFromAuth =  auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+      this.setState({currentUser: user});
       console.log(user);
       
     })
   }
 
   componentWillUnmount(){
-    this.unSubscribeFromAuth()
+    this.unsubscribeFromAuth();
   }
 
   render(){
@@ -35,7 +35,7 @@ class App extends React.Component {
 
       <BrowserRouter>    
       <div>
-      <Navbar/>
+      <Navbar currentUser={this.state.currentUser}/>
        <Switch>
           <Route exact path='/' component={Write} />
           <Route 
--- memoir/src/index.js
+++ memoir/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import SignIn from './components/SignIn';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
